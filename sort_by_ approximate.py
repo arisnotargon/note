efe83964d82278732b22a5638bc6ee116f687da6
arisnotargon <715535007@qq.com>
2020-08-31 08:06:55 +0900

    Update sort_by_ approximate.py
    优化一下快很多

--- sort_by_ approximate.py
+++ sort_by_ approximate.py
@@ -12,6 +12,7 @@ def get_primes(n):
         return 0
     isPrime = [1] * n
     isPrime[0] = isPrime[1] = 0  # 0和1不是质数，先排除掉
+
     # 埃式筛，把不大于根号n的所有质数的倍数剔除
     for i in range(2, int(n ** 0.5) + 1):
         if isPrime[i]:
@@ -24,16 +25,16 @@ if __name__ == "__main__":
     mil = 1000000
     l = get_primes(mil)
     cnt = 0
-    has = []
-    #从一百万以下最大的质数开始倍乘并压入列表,计数
+    mil_arr = [True] * mil
     for i in range(mil - 1, 0, -1):
         if l[i]:
             maxtimes = math.floor(mil / i)
             for times in range(maxtimes,0,-1):
                 prod = i * times
-                if prod not in has:
-                    has.append(prod)
+                if mil_arr[prod]:
+                    mil_arr[prod] = False
                     cnt += 1
                     print(cnt)
                     if cnt == 250001:
                         print('i===>',i,'times=>',times,'res=>',prod)
+                        exit()
